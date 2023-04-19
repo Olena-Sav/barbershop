ac848ce287f0f4a30ad71908b475358a3e4887c0
Olena Savchenko <olena.savchenko.83@gmail.com>
2023-04-19 18:00:39 +0200

    added preload.css

--- js/script.js
+++ js/script.js
@@ -6,9 +6,17 @@ const removePreload = (elem) => {
   elem.classList.remove("preload");
 };
 
+const startSlider = (slider) => {
+  // removePreload(slider);
+};
+
 const initSlider = () => {
   const slider = document.querySelector(".slider");
 
   addPreload(slider);
-  window.addEventListener("load", startSlider);
+  window.addEventListener("load", () => {
+    startSlider(slider);
+  });
 };
+
+initSlider();
