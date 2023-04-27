cd9bc51ddbaf85a60709fdaacc3e183d82da0f67
Olena Savchenko <olena.savchenko.83@gmail.com>
2023-04-27 21:03:31 +0200

    started API

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+const API_URL = "https://sudden-seemly-narwhal.glitch.me/api";
+
+/*
+GET /api - получить список услуг
+GET /api?service={n} - получить список барберов
+GET /api?spec={n} - получить список месяца работы барбера
+GET /api?spec={n}&month={n} - получить список дней работы барбера
+GET /api?spec={n}&month={n}&day={n} - получить список свободных часов барбера
+POST /api/order - оформить заказ
+*/
+
 const addPreload = (elem) => {
   elem.classList.add("preload");
 };
@@ -87,4 +98,41 @@ const initSlider = () => {
   });
 };
 
-window.addEventListener("DOMContentLoaded", initSlider);
+const renderPrice = (wrapper, data) => {
+  data.forEach((item) => {
+    const priceItem = document.createElement("li");
+    priceItem.classList.add("price__item");
+
+    priceItem.innerHTML = `
+      <span class="price__item-title">${item.name}</span>
+      <span class="price__item-count">${item.price} руб</span>
+    `;
+
+    wrapper.append(priceItem);
+  });
+};
+
+const initService = () => {
+  const priceList = document.querySelector(".price__list");
+  priceList.textContent = "";
+
+  addPreload(priceList);
+
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      renderPrice(priceList, data);
+      removePreload(priceList);
+      return data;
+    });
+    .then(data => {
+      renderService()
+    })
+};
+
+const init = () => {
+  initSlider();
+  initService();
+};
+
+window.addEventListener("DOMContentLoaded", init);
